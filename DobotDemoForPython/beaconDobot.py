c9c742bbf205ba642eecd8bf1023081dd0cb3ef0
outan <outannexway@gmail.com>
2017-03-04 23:01:05 +0900

    add speech recgnition

--- DobotDemoForPython/beaconDobot.py
+++ DobotDemoForPython/beaconDobot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import DobotDllType as dType
 import atexit
 
@@ -25,7 +26,7 @@ def all_done():
 
 atexit.register(all_done)
 
-def on_message(*args):
+def on_message_drink(*args):
     order = args[0]
     print('order from ble_button is: ' + order)
     if (dobotConnecResult == 0):
@@ -101,5 +102,131 @@ def on_connect_audio():
 
 socketIO_edison = SocketIO('edison1_pepper.local', 3000, LoggingNamespace)
 socketIO_edison.on('connect', on_connect)
-socketIO_edison.on('ble_button', on_message)
+socketIO_edison.on('drink_button', on_message_drink)
+
+
+#マイクからの入力を5秒間録音し、ファイル名：voice.wavで保存する。
+import requests
+import pyaudio
+import sys
+import time
+import wave
+import os.path
+import json
+
+try:
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+    )
+    import apiai
+CLIENT_ACCESS_TOKEN = '3d93f36cf47f421386d517df3be45c07'
+
+chunk = 1024*2
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+#サンプリングレート、マイク性能に依存
+RATE = 16000
+#録音時間
+RECORD_SECONDS =  4
+
+def on_message_speech_order(*args):
+    print("get into on_message_speech_order")
+    mode = args[0]
+    if mode == "speech":
+        print 'マイクに4秒間話しかけてください >>>'
+        p = pyaudio.PyAudio()
+        input_device_index = 0  #マイク0番を設定
+        stream = p.open(  #マイクからデータ取得
+            format = FORMAT,
+            channels = CHANNELS,
+            rate = RATE,
+            input = True,
+            input_device_index = input_device_index,
+            frames_per_buffer = chunk
+            )
+
+        all = []
+        for i in range(0, RATE / chunk * RECORD_SECONDS):
+                data = stream.read(chunk)
+                all.append(data)
+
+        stream.close()
+        data = ''.join(all)
+        out = wave.open('voice.wav','w')
+        out.setnchannels(1) #mono
+        out.setsampwidth(2) #16bit
+        out.setframerate(RATE)
+        out.writeframes(data)
+        out.close()
+
+        p.terminate()
+
+        print '<<< 録音完了'
+
+        path = 'voice.wav'
+        APIKEY = '484232446270726f464a6c3831737349657478796a4654717970364859574458656f78552e3974686b4631'
+        url = "https://api.apigw.smt.docomo.ne.jp/amiVoice/v1/recognize?APIKEY={}".format(APIKEY)
+        files = {"a": open(path, 'rb'), "v":"on"}
+        print("files is ", files)
+        r = requests.post(url, files=files)
+        print("order text is: ")
+        print r.json()['text']
+        text = r.json()['text']
+
+        water = "水".decode("utf-8")
+        tee = "茶".decode("utf-8")
+        coffee = "コーヒー".decode("utf-8")
+
+        if water in text:
+            socketIO_edison.emit('speech_order', 'water')
+            print("emit speech_order: " + 'water')
+            get_drink("water")
+        elif tee in text:
+            socketIO_edison.emit('speech_order', 'tee')
+            print("emit speech_order: " + 'tee')
+            get_drink("tee")
+        elif coffee in text:
+            socketIO_edison.emit('speech_order', 'coffee')
+            print("emit speech_order: " + 'coffee')
+            get_drink("coffee")
+        else:
+            socketIO_edison.emit('speech_order', 'notUnderstand')
+            print("emit speech_order: " + 'notUnderstand')
+
+        # ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+        #
+        # request = ai.text_request()
+        #
+        # request.lang = 'ja'  # optional, default value equal 'en'
+        #
+        # request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+        #
+        # request.query = text
+        #
+        # response1 = request.getresponse().read()
+        # print ("response1 is: ")
+        # print(response1)
+        # response2 = json.loads(response1)
+        # print ("response2 is: ")
+        # print(response2)
+        #
+        # intent = response2['result']['metadata']['intentName']
+        # print("intent is: ")
+        # print(intent)
+        # drinks = ['water', 'tee', 'coffee']
+        # if intent in drinks:
+        #   get_drink(intent)
+        # elif intent == "Default Fallback Intent":
+        #     #speech = response2['result']['fulfillment']['messages']['speech']
+        #     speech = response2['result']['fulfillment']['messages'][0]['speech']
+        #     print(speech)
+        #     socketIO_edison.emit('notUnderstand', speech)
+        #     print("emit notUnderstand:" + speech)
+
+
+socketIO_edison.on('speech_order_button',on_message_speech_order)
+socketIO_edison.on('speech_once_again',on_message_speech_order)
+
 socketIO_edison.wait()
